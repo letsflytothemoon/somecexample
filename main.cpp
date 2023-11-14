b2d99ecea5e941c9d351f9001cd49c1ecf8c6bd7
Dmitriy Losev <losev3628@gmail.com>
2023-11-14 20:27:23 +1000

    init

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include "somec/somec.h"
+
+struct BlackBody : std::ostream
+{
+    BlackBody& operator <<(std::ostream&(*)(std::ostream&)) { return *this; }
+    template <class T> BlackBody& operator <<(T) { return *this; }
+};
+
+
+struct COpts
+{
+    static const int BITS    = 16;
+    static const int REGS    = 2;
+    static const int RAMSIZE = 512;
+    
+    static const char* regname(int index)
+    {
+        static const char* names[] = {"a", "b", "c", "d"};
+        return names[index];
+    }
+    
+    static std::ostream& GetBlackBody()
+    {
+        static BlackBody value;
+        return value;
+    }
+    
+    static std::ostream& asm_ostr()
+    { return std::cout; }
+    
+    static std::ostream& code_ostr()
+    { return GetBlackBody(); }
+    
+    static std::ostream& verilog_ostr()
+    { return GetBlackBody(); }
+};
+
+typedef SomeC<COpts>   C;
+typedef C::StackStrat  Stack;
+typedef C::StaticStrat Static;
+
+template <class T>
+using Ref = C::Ref<T>;
+
+template <class T>
+using Var = C::Var<T>;
+
+int main()
+{
+    Var<Stack> i = 10;
+    Ref<Stack> ref = i;
+    
+    ref = 27;
+    
+    return 0;
+}
